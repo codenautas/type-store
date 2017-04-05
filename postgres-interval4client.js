c75ec8fb8c28d8b84fab4bcb65a6e877604a0610
estefi-capece <eycapece@gmail.com>
2017-04-05 16:25:48 -0300

    Subo el interval4client.js

---
+++ postgres-interval4client.js
@@ -0,0 +1,29 @@
+"use strict";
+
+(function codenautasModuleDefinition(root, name, factory) {
+    /* global define */
+    /* istanbul ignore next */
+    if(typeof root.globalModuleName !== 'string'){
+        root.globalModuleName = name;
+    }
+    /* istanbul ignore next */
+    if(typeof exports === 'object' && typeof module === 'object'){
+        module.exports = factory();
+    }else if(typeof define === 'function' && define.amd){
+        define(factory);
+    }else if(typeof exports === 'object'){
+        exports[root.globalModuleName] = factory();
+    }else{
+        root[root.globalModuleName] = factory();
+    }
+    root.globalModuleName = null;
+})(/*jshint -W040 */this, 'PostgresInterval', function() {
+/*jshint +W040 */
+
+function PostgresInterval(){};
+
+PostgresInterval["4client"]=true;
+
+return PostgresInterval;
+
+});
